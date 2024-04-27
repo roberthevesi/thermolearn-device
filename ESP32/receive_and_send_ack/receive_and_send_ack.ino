71f2b1080de9db01788941d5d84ddc753d6e3783
Robert Hevesi <roberthevesi09@gmail.com>
2024-04-27 20:31:04 +0300

    Added status request capability

--- ESP32/receive_and_send_ack/receive_and_send_ack.ino
+++ ESP32/receive_and_send_ack/receive_and_send_ack.ino
@@ -4,6 +4,8 @@
 #error Bluetooth not enabled!
 #endif
 
+const int ledPin = 21;
+
 BluetoothSerial SerialBT;
 
 void setup() {
@@ -11,6 +13,13 @@ void setup() {
   Serial.begin(115200);
   SerialBT.begin("ESP32_BT");
   Serial.println("The device started, you can now pair it using Bluetooth.");
+
+  pinMode(ledPin, OUTPUT);
+}
+
+void send_ack(String message){
+  SerialBT.println("ACK_" + message);  // Acknowledgment message
+  Serial.println("Sent ACK for " + message);
 }
 
 void loop() {
@@ -18,11 +27,29 @@ void loop() {
     String message = SerialBT.readString();
     Serial.println("Received: " + message);
 
+    if(message == "stat_req"){
+      int status = digitalRead(ledPin);
 
-    //if (message == "ON" || message == "OFF") {
-      SerialBT.println("ACK_" + message + " FROM ESP:)");  // Acknowledgment message
-      Serial.println("Sent ACK for " + message);
-    //}
+      if(status == 0){
+        Serial.println("status 0");
+        SerialBT.println("OFF");
+      }
+      else if(status == 1){
+        Serial.println("status 1");
+        SerialBT.println("ON");
+      }
+    }
+    else if(message == "ON"){
+      digitalWrite(ledPin, HIGH);
+      send_ack(message);
+    }
+    else if(message == "OFF"){
+      digitalWrite(ledPin, LOW);
+      send_ack(message);
+    }
+    else{
+      send_ack("unknown");
+    }
   }
   delay(20);
 }
