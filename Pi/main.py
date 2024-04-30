f784bde3b0d283043cb11bdc8c70d6c7b04a3cbb
Robert Hevesi <roberthevesi09@gmail.com>
2024-04-30 21:38:28 +0300

    Added ACK system on Pi

--- Pi/main.py
+++ Pi/main.py
@@ -16,15 +16,37 @@ sock.connect((esp32_address, port))
 
 # FUNCTION TO SEND BLUETOOTH MESSAGE
 def send_bluetooth_message(message):
-    sock.send(message)
-    print(f"Sent: {message}")
-    response = sock.recv(1024)
-    received = response.decode('utf-8')
-    print(f"Received: {received}")
+    try:
 
-    if "ACK_"+message == received:
-        return True
-    return False
+        sock.send(message + '!')
+        print(f"-> Sent: <{message + '!'}>")
+
+        received_data = ""
+
+        while True:
+            data = sock.recv(1024)
+            if not data:
+                print("no data...")
+                break
+            received_data += data.decode('utf-8').strip()
+            print(f"<- Received: <{received_data}>")
+
+            if received_data.endswith('!'):
+                if message == 'stat_req':
+                    return received_data[:-1]
+                
+                if "ACK_" + message + '!' == received_data:
+                    return message[:-1]
+                else:
+                    return None
+            else:
+                continue
+
+    except bluetooth.BluetoothError as e:
+        print(f"Bluetooth Error: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
 
 # FUNCTION TO GET THING SHADOW
 def get_iot_core_thing_shadow():
@@ -36,59 +58,69 @@ def get_iot_core_thing_shadow():
 
     shadow_client.disconnect()
 
+
 def ping_shadow():
     shadow_result = get_iot_core_thing_shadow()
     return shadow_result
     # mode = shadow_result.get('mode')
     # desiredTemp = shadow_result.get('desiredTemp')
 
-# FUNCTION TO GET LAST HEATING STATUS
-def get_status():
-    message = 'stat_req'
 
-    sock.send(message)
-    print(f"Sent: {message}")
-
-    response = sock.recv(1024)
-    received = response.decode('utf-8')
-    print(f"Received: {received}")
-
-    return received
+def get_environment_temp():
+    max_tries = 10
+    while max_tries:
+        try:
+            temp = dht_device.temperature
+            if temp is not None:
+                return temp
+        except RuntimeError as e:
+            print(f"@@@@@ An error occurred: {e}")
+        finally:
+            max_tries -= 1     
+            time.sleep(1)  
+                                                                  
 
 if __name__ == "__main__":
-    last_status = get_status()
+    message = 'stat_req'
+    status = None
+
+    while status == None:
+        status = send_bluetooth_message(message)
+        time.sleep(1)
+
 
     while True:
         try:
             shadow_result = get_iot_core_thing_shadow()
             mode = shadow_result.get('mode')
             desiredTemp = shadow_result.get('desiredTemp')
-            print(f"Desired: {desiredTemp}")
 
-            temp_celsius = dht_device.temperature
+            print(f"- Desired: {desiredTemp}")
+
+            temp_celsius = get_environment_temp()
+
             if temp_celsius is not None:
-                print(f"Current temp: {temp_celsius:.1f}")
+                print(f"- Current temp: {temp_celsius:.1f}")
 
                 if temp_celsius < desiredTemp:
-                    if last_status == 'OFF':
-                        last_status = 'ON'
-                        while send_bluetooth_message(last_status) == False:
+                    if status == 'OFF':
+                        status = 'ON'
+                        while send_bluetooth_message(status) == None:
                             time.sleep(1)
                             continue
                 else:
-                    if last_status == 'ON':
-                        last_status = 'OFF'
-                        while send_bluetooth_message(last_status) == False:
+                    if status == 'ON':
+                        status = 'OFF'
+                        while send_bluetooth_message(status) == None:
                             time.sleep(1)
                             continue
             else:
                 print("failed to read temp.")
 
-        
-
         except RuntimeError as e:
             print(f"An error occurred: {e}")
 
+        print('\n')
         time.sleep(2)
 
     sock.close()
