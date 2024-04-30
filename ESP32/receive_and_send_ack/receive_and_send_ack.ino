fc1f7114e0c12a5806e913cff06b38a198653b33
Robert Hevesi <roberthevesi09@gmail.com>
2024-04-30 21:42:55 +0300

    Improved ACK system on ESP

--- ESP32/receive_and_send_ack/receive_and_send_ack.ino
+++ ESP32/receive_and_send_ack/receive_and_send_ack.ino
@@ -18,26 +18,46 @@ void setup() {
 }
 
 void send_ack(String message){
-  SerialBT.println("ACK_" + message);  // Acknowledgment message
-  Serial.println("Sent ACK for " + message);
+  String send = "ACK_" + message + '!';
+  SerialBT.println(send);  // Acknowledgment message
+  Serial.println("-> Sent: <" + send + ">");
+  SerialBT.flush();
+}
+
+String readMessage(){
+  String message = "";
+  char ch;
+  while(SerialBT.available()){
+    ch = SerialBT.read();
+    if(ch == '!')
+      break;
+    message += ch;
+  }
+  SerialBT.flush();
+  return message;
 }
 
 void loop() {
   if (SerialBT.available()) {
-    String message = SerialBT.readString();
-    Serial.println("Received: " + message);
+    String message = readMessage();
+    // String message = SerialBT.readString();
+    // SerialBT.flush();
+    Serial.println("<- Received: <" + message + ">");
 
     if(message == "stat_req"){
       int status = digitalRead(ledPin);
 
       if(status == 0){
         Serial.println("status 0");
-        SerialBT.println("OFF");
+        SerialBT.println("OFF!");
+        SerialBT.flush();
       }
       else if(status == 1){
         Serial.println("status 1");
-        SerialBT.println("ON");
+        SerialBT.println("ON!");
+        SerialBT.flush();
       }
+
     }
     else if(message == "ON"){
       digitalWrite(ledPin, HIGH);
@@ -51,5 +71,5 @@ void loop() {
       send_ack("unknown");
     }
   }
-  delay(20);
+  delay(100);
 }
