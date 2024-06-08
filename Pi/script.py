30551d2a3240f359fea9efee5086c17371c8d876
roberthevesi <roberthevesi09@gmail.com>
2024-06-08 11:43:48 +0300

    Modified services, main script. Integrated ML decision.

--- Pi/script.py
+++ Pi/script.py
@@ -1,6 +1,10 @@
 from EC2Service import is_thermostat_paired, get_thermostat_schedule, save_thermostat_status_log, get_user_distance_from_home, get_lastest_user_log
 from InternetService import is_connected_to_internet, get_current_day_and_time
 from BluetoothService import turn_on_thermostat, turn_off_thermostat, get_thermostat_status
+
+from IncrementalModelUpdateService import update_model_periodically
+from PredictionService import predict_next_home_time
+
 import IoTCoreService
 import time
 from datetime import datetime, timedelta
@@ -26,15 +30,16 @@ weekly_schedule = {
 }
 days_of_week = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY']
 
-DISTANCE_FROM_HOME_THRESHOLD = 20
-TARGET_TEMP_THRESHOLD = 22
+DISTANCE_FROM_HOME_THRESHOLD = 500 # meters
+NEXT_TIME_HOME_THRESHOLD = 1 # hours
+AWAY_FROM_HOME_COMFORT_TEMPERATURE = 21 # degrees Celsius
 
 class ThermostatStateMachine:
     def __init__(self):
         self.state = 'Init'
 
-
         self.targetTemp = 0
+        self.targetTempAux = 0
         self.environmentTemp = 0
         self.environmentHumidity = 0
         self.thermostatStatus = None
@@ -56,7 +61,7 @@ class ThermostatStateMachine:
         self.init_finished = False
 
     def transition(self, new_state):
-        print(f"<<< Transitioning from {self.state} to {new_state} >>>")
+        print(f"<<<<< Transitioning from {self.state} to {new_state} >>>>>")
         self.state = new_state
         self.run_state()
 
@@ -68,7 +73,6 @@ class ThermostatStateMachine:
             self.get_current_time_day()
             self.start_clock_thread()
             self.start_listening()
-            # self.init_finished = True
         elif self.state == 'ComparingRequests':
             self.compare_requests()
         elif self.state == 'ReadingEnvironmentTemperature':
@@ -94,6 +98,7 @@ class ThermostatStateMachine:
                 print("Thermostat is not connected to the internet")
             time.sleep(10)
 
+
     def check_thermostat(self):
         global thermostatId
         print("\n--- Checking if thermostat is paired ---")
@@ -103,7 +108,6 @@ class ThermostatStateMachine:
                 print("Thermostat is paired")
                 thermostatId = response.get('id')
                 IoTCoreService.thermostatId = thermostatId
-                # self.transition('FetchingTimeDay')
                 break
             else:
                 print("Thermostat is not paired")
@@ -120,7 +124,18 @@ class ThermostatStateMachine:
         print("\n--- Fetching current time and day ---")
         self.current_day_of_week, self.current_time, self.current_date = get_current_day_and_time()
         self.current_day_index = days_of_week.index(self.current_day_of_week)
-        self.current_datetime = datetime.strptime(self.current_time, '%H:%M:%S')
+
+        date_part = datetime.strptime(self.current_date, '%Y-%m-%d').date()
+        time_part = datetime.strptime(self.current_time, '%H:%M:%S').time()
+
+        self.current_datetime = datetime.combine(date_part, time_part)
+
+        print(f"Current day: {self.current_day_of_week}")
+        print(f"Current time: {self.current_time}")
+        print(f"Current date: {self.current_date}")
+        print(f"Current day index: {self.current_day_index}")
+        print(f"Current datetime: {self.current_datetime}")
+
 
     def start_clock(self):
         while True:
@@ -131,10 +146,12 @@ class ThermostatStateMachine:
             if self.current_datetime.hour == 0 and self.current_datetime.minute == 0 and self.current_datetime.second == 0:
                 self.current_day_index = (self.current_day_index + 1) % 7
 
+
     def start_clock_thread(self):
         clock_thread = threading.Thread(target=self.start_clock)
         clock_thread.daemon = True
         clock_thread.start()
+
 
     def start_listening(self):
         print("\n--- Starting listening ---")
@@ -164,6 +181,7 @@ class ThermostatStateMachine:
         if self.init_finished is not False:
             self.transition('ComparingRequests')
 
+
     def fetch_schedule(self):
         print("--- Fetching thermostat schedule...")
         response = get_thermostat_schedule()
@@ -172,10 +190,12 @@ class ThermostatStateMachine:
         for day, entries in weekly_schedule.items():
             print(f"{day}: {entries}")
 
+
     def reset_schedule(self):
         global weekly_schedule
         for day in weekly_schedule:
             weekly_schedule[day] = []
+
 
     def populate_schedule(self, response):
         global weekly_schedule
@@ -187,15 +207,11 @@ class ThermostatStateMachine:
                     'desiredTemperature': entry['desiredTemperature']
                 })
 
+
     ######################## 1 - ComparingRequests STATE ########################
 
     def compare_requests(self):
-        # if self.init_finished is not False: 
-        #     return
-        
         print("\n--- Comparing manual request with scheduled events ---")
-
-        # if self.instant_request_timestamp 
 
         last_scheduled_event = self.get_last_scheduled_event()
         print("last_scheduled_event: ", last_scheduled_event)
@@ -245,9 +261,18 @@ class ThermostatStateMachine:
         for day_offset in range(7):
             day_index = (self.current_day_index - day_offset) % 7
             day_name = days_of_week[day_index]
+            print("Searching for day: ", day_name)
             for event in weekly_schedule[day_name]:
                 event_time = datetime.strptime(event.get('startTime'), '%H:%M:%S').time()
-                if event_time <= self.current_time:
+                if day_name == self.current_day_of_week:
+                    if event_time <= self.current_time:
+                        most_recent_event = event
+                        most_recent_event['day_of_the_week'] = day_name
+
+                        print("most recent event found, ", most_recent_event)
+
+                        return most_recent_event
+                else:
                     most_recent_event = event
                     most_recent_event['day_of_the_week'] = day_name
 
@@ -290,6 +315,7 @@ class ThermostatStateMachine:
         else:
             return -1
     
+
     ######################## 2 - ReadingEnvironmentTemperature STATE ########################
 
     def read_environment_temperature(self):
@@ -357,68 +383,101 @@ class ThermostatStateMachine:
         self.transition('ListeningForNewEvents')
 
 
-    ######################## 4 - ListeningForNewEvents STATE #######################
+    ######################## 4 - ListeningForNewEvents STATE ########################
 
     def listen_for_events(self):    
         print("\n--- Listening for new events ---")
         pass
+
+
+    ######################## REGULAR TEMPERATURE CHECK THREAD ########################
 
     def start_regular_temperature_check(self):
         while True:
             time.sleep(30) # check temp every 30s
             self.transition('ReadingEnvironmentTemperature')
 
-    def start_regular_user_location_check(self):
+
+    ######################## PERIODIC USER CHECK THREAD ########################
+
+    def start_periodic_user_check(self):
         while True:
-            time.sleep(5) # check temp every 2m
-            print("Checking user location...")
+            time.sleep(2 * 60) # check user location every 2min
+            print("--- Checking user location every 2 minutes ---")
 
             latest_user_log = get_lastest_user_log().get('eventType')
             print("Latest user log: ", latest_user_log)
+
             if latest_user_log == 'OUT':
                 distance_from_home = int(get_user_distance_from_home())
                 print("Distance from home: ", distance_from_home)
+
                 if distance_from_home > DISTANCE_FROM_HOME_THRESHOLD:
-                    if float(self.targetTemp) > TARGET_TEMP_THRESHOLD:
-                        # still have to check ML output
-                        print("Setting target temp to 21 for now...")
+
+                    if float(self.targetTemp) >= AWAY_FROM_HOME_COMFORT_TEMPERATURE:
+                        
+                        next_home_time = predict_next_home_time()
+                        print("Next home time: ", next_home_time)
+
+                        time_difference = next_home_time - self.current_datetime
+
+                        if time_difference > timedelta(hours=NEXT_TIME_HOME_THRESHOLD):
+                            print("User won't be home in the next 2 hours.")
+                            if self.targetTempAux == 0:
+                                self.targetTempAux = self.targetTemp
+                                self.targetTemp = AWAY_FROM_HOME_COMFORT_TEMPERATURE
+                                print(f"Setting the temperature to {AWAY_FROM_HOME_COMFORT_TEMPERATURE}.")
+
+                        else:
+                            print("User will be home in less than 2 hours.")
+                            self.check_temperature()
+
                     else:
-                        print("Target temp is not high enough. No changes.")
+                        print("Target temp is not high enough. ")
+                        self.check_temperature()
+
                 else:
-                    print("User is near home. No changes.")
+                    print("User is near home. ")
+                    self.check_temperature()
+
             else:
-                print("User is at home. No changes.")
+                print("User is at home.")
+                self.check_temperature()
+                
+
+    def check_temperature(self):
+        if self.targetTempAux != 0:
+            self.targetTemp = self.targetTempAux
+            self.targetTempAux = 0
+            print("Setting the temperature back to their preference.")
+        else:
+            print("No changes.")
 
 
+    ######################## PERIODIC INCREMENTAL UPDATE THREAD ########################
 
-            # distance_from_home = get_user_distance_from_home()
-            # print("Distance from home: ", distance_from_home)
-            # if distance_from_home > DISTANCE_FROM_HOME_THRESHOLD:
-            #     print("User is far from home. Turning off thermostat...")
-            #     if self.thermostatStatus == 'ON':
-            #         self.thermostatStatus = 'OFF'
-            #         save_thermostat_status_log('OFF', self.environmentTemp, self.environmentHumidity)
-            #         turn_off_thermostat()
-            # else:
-            #     print("User is near home. Turning on thermostat...")
-            #     if self.thermostatStatus == 'OFF':
-            #         self.thermostatStatus = 'ON'
-            #         save_thermostat_status_log('ON', self.environmentTemp, self.environmentHumidity)
-            #         turn_on_thermostat()
-
+    def periodic_incremental_update(self):
+        while True:
+            time.sleep(60 * 60 * 24) # periodic model update every 24h
+            print("Updating model every 24 hours...")
+            update_model_periodically()
 
 
 if __name__ == "__main__":
     thermostat = ThermostatStateMachine()
     thermostat.run_state()
 
-    temperature_check_thread = threading.Thread(target=thermostat.start_regular_temperature_check)
-    temperature_check_thread.daemon = True
-    temperature_check_thread.start()
+    regular_temperature_check_thread = threading.Thread(target=thermostat.start_regular_temperature_check)
+    regular_temperature_check_thread.daemon = True
+    regular_temperature_check_thread.start()
 
-    user_location_check_thread = threading.Thread(target=thermostat.start_regular_user_location_check)
-    user_location_check_thread.daemon = True
-    user_location_check_thread.start()
+    periodic_user_check_thread = threading.Thread(target=thermostat.start_periodic_user_check)
+    periodic_user_check_thread.daemon = True
+    periodic_user_check_thread.start()
+
+    periodic_incremental_update_thread = threading.Thread(target=thermostat.periodic_incremental_update)
+    periodic_incremental_update_thread.daemon = True
+    periodic_incremental_update_thread.start()
 
     while True:
         time.sleep(5)
